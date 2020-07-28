7d23d0cbce256c9095c8a4f65dda4086e25fe497
alinashev <al.t.shevchenko@gmail.com>
2020-07-28 22:40:30 +0300

    first task

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -1,12 +1,117 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-namespace ConsoleApp8
+namespace Task1
 {
     class Program
     {
+        static void WordRemover(string path)
+        {
+            Console.WriteLine("Подстрока, которую нужно вырезать:");
+            string subString = Console.ReadLine();
+            string newPath = @"" + path.Replace(".", "copy.");
+
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.CopyTo(newPath, true);
+            }
+            
+            string readText = File.ReadAllText(path);
+            try
+            {
+                string createText = readText.ToLower().Replace(subString, "");
+                File.WriteAllText(path, createText);
+            }
+            catch
+            {
+                Console.WriteLine("Такой подстроки нету!");
+            }
+            
+        }
+
+        static void  AmountAndWords(string path)
+        {
+            string readText = File.ReadAllText(path);
+            string[] words = readText.Split(new char[] {' ','"', '.', '?', '!', ',' , '(', ')'}, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine("Количество слов:" + words.Length);
+            List<string> resultWords = new List<string>();
+            string word;
+            for (int i = 9; i < words.Length; i += 10 )
+            {
+                word = words[i];
+                resultWords.Add(word);
+            }
+            Console.WriteLine(String.Join(',', resultWords));
+        }
+
+        static void PrintSentance(string path)
+        {
+            string readText = File.ReadAllText(path);
+            string [] sentences = readText.Split(new char[] {'.'}, StringSplitOptions.RemoveEmptyEntries);
+            string senrence = sentences[2]; 
+            string[] words = senrence.Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+            string word; 
+            List<string> reverseWords = new List<string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                word = words[i];
+                reverseWords.Add(new string(word.ToCharArray().Reverse().ToArray()));
+            }
+            Console.WriteLine(String.Join(separator: ' ', reverseWords));
+        }
+           
+        static void DirectoryProcessing(string directoryName)
+        {
+            while(true)
+            {
+                string[] dirs = Directory.GetDirectories(directoryName);
+                string[] fls = Directory.GetFiles(directoryName); 
+                Dictionary<int, string> dirеctory = new Dictionary<int, string>();
+
+                if (Directory.Exists(directoryName))
+                {
+                    for(int i = 0; i < dirs.Length; i++) {dirеctory.Add(i+1, dirs[i]); }
+                    for(int i = 0; i < fls.Length; i++) {dirеctory.Add(i+dirs.Length + 1, fls[i]); }
+                
+                    foreach (var d in dirеctory)
+                    {
+                        Console.WriteLine("{0} - {1}", d.Key, d.Value);
+                    }
+                }
+                int select = Convert.ToInt32(Console.ReadLine( ));
+                directoryName = dirеctory[select];
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1 - Вызвать метод 1\n2 - Вызвать метод 2\n3 - Вызвать метод 3\n4 - Вызвать метод 4");
+            string select = Console.ReadLine();
+            
+            Console.WriteLine("Введите путь: ");
+            string pathToFile = @"" + Console.ReadLine();
+            
+            switch(select)
+            {
+                case "1":
+                    WordRemover(pathToFile);
+                    break;
+                case "2":
+                    AmountAndWords(pathToFile);
+                    break;
+                case "3":
+                    PrintSentance(pathToFile);
+                    break;
+                case "4":
+                    DirectoryProcessing(pathToFile);
+                    break;
+                default:
+                    Console.WriteLine("Некорректный ввод!");
+                    break;
+            }
         }
     }
 }
