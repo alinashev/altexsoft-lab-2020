56e860596d63156dc7820e1932fdce616c6e4550
alinashev <al.t.shevchenko@gmail.com>
2020-09-03 18:16:15 +0300

    Added repository(fixed)

---
+++ task2/Controller/RecipeController/UpdaterAllIngredients.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Task2App
+{
+    public class UpdaterAllIngredients
+    {
+        private  readonly FileWriter _fileWriter;
+
+        public UpdaterAllIngredients(FileWriter _fileWriter)
+        {
+            this._fileWriter = _fileWriter;
+        }
+
+        public void AddIngredientsToAllList(Dictionary<string, string> currentIngredients, string directoryPath)
+        {
+            Dictionary<string, string> allFromJson =
+                JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(directoryPath));
+            currentIngredients.ToList().ForEach(x => allFromJson.Add(x.Key, x.Value));
+            _fileWriter.Serialize(directoryPath, allFromJson);
+        }
+    }
+}
---
+++ task2/FileWriter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Task2App
+{
+    public class FileWriter
+    {
+        private List<string> _dirsList = new List<string>();
+        public void Serialize(string path, object json)
+        {
+            if (CheckDirectory(path))
+            {
+                File.WriteAllText(path, JsonConvert.SerializeObject(json, Formatting.Indented));
+            }
+            else
+            {
+                Console.WriteLine("Path does not exist");
+            }
+            
+        }
+        private bool CheckDirectory(string path)
+        {
+            string[] _dirsAndFiles = path.Split(@"\");
+            for (int i = 0; i < _dirsAndFiles.Length-1; i++)
+            {
+                _dirsList.Add(_dirsAndFiles[i]);
+            }
+
+            string checkedPath = string.Join(@"\", _dirsList);
+            checkedPath = checkedPath.Substring(checkedPath.Length / 2);
+            if (Directory.Exists(checkedPath))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
